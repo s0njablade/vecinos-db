e1fa012a8d8df1eb0feb86176e00d5a3b68bc84e
s0njablade <sonjita.chacon@gmail.com>
2019-01-22 12:13:20 -0700

    attempting deployment

--- database-connection.js
+++ database-connection.js
@@ -1,4 +1,4 @@
-const environment = process.env.NODE_ENV || 'deployment'
+const environment = process.env.NODE_ENV || 'development'
 const config = require('./knexfile')
 const configEnv = config[environment]
 const knex = require('knex')
